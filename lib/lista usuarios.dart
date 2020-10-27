aa2803e1e665541ade53900abb62f44153409ab0
acmsao <acmsao@gmail.com>
2020-10-27 15:02:58 -0300

    popup apagar usuarios

--- lib/lista usuarios.dart
+++ lib/lista usuarios.dart
@@ -17,68 +17,105 @@ class _ListaUsuariosState extends State<ListaUsuarios> {
     var snapshots = Firestore.instance.collection('usuarios').snapshots();
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Usuarios'),
-        ),
-        body: StreamBuilder(
-          stream: snapshots,
-          builder:
-              (BuildContext context, AsyncSnapshot<QuerySnapshot> snaphots) {
-            if (snaphots.hasError) {
-              return Center(child: Text('Erro ${snaphots.error}'));
-            }
+      appBar: AppBar(
+        title: Text('Usuarios'),
+      ),
+      body: StreamBuilder(
+        stream: snapshots,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snaphots) {
+          if (snaphots.hasError) {
+            return Center(child: Text('Erro ${snaphots.error}'));
+          }
 
-            if (snaphots.connectionState == ConnectionState.waiting) {
-              return Center(child: CircularProgressIndicator());
-            }
+          if (snaphots.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
 
-            if (snaphots.data.documents.length == 0) {
-              return Center(child: Text("Lista Vazia"));
-            }
+          if (snaphots.data.documents.length == 0) {
+            return Center(child: Text("Lista Vazia"));
+          }
 
-            return ListView.separated(
-              itemCount: snaphots.data.documents.length,
-              itemBuilder: (context, int i) {
-                var item = snaphots.data.documents[i].data;
+          return ListView.separated(
+            itemCount: snaphots.data.documents.length,
+            itemBuilder: (context, int i) {
+              var item = snaphots.data.documents[i].data;
 
-                return Container(
-                  padding: const EdgeInsets.all(8),
-                  child: ListTile(
-                    leading: Text(item['nome']),
-                    trailing: Container(
-                      width: 100,
-                      child: Row(
-                        children: <Widget>[
-                          Text(item['tipo']),
-                          IconButton(
-                            icon: Icon(Icons.delete),
-                            onPressed: () {
-                              Deletar(snaphots.data.documents[i].documentID,
-                                  'usuarios');
-                            },
-                            color: Colors.red,
-                          ),
-                        ],
-                      ),
+              return Container(
+                padding: const EdgeInsets.all(8),
+                child: ListTile(
+                  leading: Text(item['nome']),
+                  trailing: Container(
+                    width: 100,
+                    child: Row(
+                      children: <Widget>[
+                        Text(item['tipo']),
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => AlertDialog(
+                                    title: Text('Apagar Item'),
+                                    content: SingleChildScrollView(
+                                      child: ListBody(
+                                        children: <Widget>[
+                                          Text(
+                                              'Deseja realmente apagar este usuario?',
+                                              style: TextStyle(fontSize: 20)),
+                                          Padding(padding: EdgeInsets.all(20)),
+                                          Text('Esta ação é irreversível'),
+                                        ],
+                                      ),
+                                    ),
+                                    actions: <Widget>[
+                                      FlatButton(
+                                          onPressed: () {
+                                            Deletar(
+                                                snaphots.data.documents[i]
+                                                    .documentID,
+                                                'usuarios');
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: Text('Apagar',
+                                              style: TextStyle(fontSize: 20))),
+                                      Padding(padding: EdgeInsets.all(40)),
+                                      FlatButton(
+                                        child: Text(
+                                          'Voltar',
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ));
+                          },
+                          color: Colors.red,
+                        ),
+                      ],
                     ),
                   ),
-                );
-              },
-              separatorBuilder: (context, index) => const Divider(),
-            );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(onPressed: (){
-        
-         Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => AdicionarUsu(),
-              ),
-            );
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => const Divider(),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AdicionarUsu(),
+            ),
+          );
         },
         child: Icon(Icons.add),
-        ),
-        );
+      ),
+    );
   }
 }
